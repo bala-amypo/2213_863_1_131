e793aa770a933f1382327857987e79d052bd912e
bala-amypo <bala@amypo.in>
2025-12-22 08:47:44 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/SupplyForecastServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/SupplyForecastServiceImpl.java
@@ -3,12 +3,10 @@ package com.example.demo.service.impl;
 import com.example.demo.entity.SupplyForecast;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.SupplyForecastRepository;
-import com.example.demo.service.SupplyForecastService;
-import lombok.RequiredArgsConstructor;
+import com.example.demo.service.SupplyForecastService; 
 import org.springframework.stereotype.Service;
 
-@Service
-@RequiredArgsConstructor
+@Service 
 public class SupplyForecastServiceImpl implements SupplyForecastService {
 
     private final SupplyForecastRepository supplyForecastRepository;
